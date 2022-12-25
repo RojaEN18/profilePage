7789999b27b0794b48a782e3f0fe641e822a36a1
RojaEN18 <96619041+RojaEN18@users.noreply.github.com>
2022-12-25 22:29:36 +0530

    Update index.php

--- index.php
+++ index.php
@@ -97,6 +97,6 @@ echo ' <div id="reg-head-fail" class="headrg">Login Failed!, Invalid Credentials
 </div>
 </div>
 </div>
-<div id="footer"><p> Copyright &copy; 2022 FresherProfiles.com </p></div>
+<div id="footer"><p> Copyright &copy; 2022 Profiles.com </p></div>
 </body>
 </html>
